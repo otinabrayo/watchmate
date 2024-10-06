4f180c395d224f6160695894995d92f46aed0199
Brian Otina <brianotina20@gmail.com>
2024-10-06 23:41:04 +0300

    ..

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -3,10 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-od(-287^b@b3hqtdam2$c-28nlagtivxku%aj+$@)9efd%zy9$'
 
