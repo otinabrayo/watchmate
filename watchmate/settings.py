0f21f277eb2e816cbafa5c982b95a6e642e25ce6
Brian Otina <brianotina20@gmail.com>
2024-10-11 22:17:37 +0300

    ...

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -49,10 +49,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'watchmate.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
